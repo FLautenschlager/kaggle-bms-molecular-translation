622e8cdf6aa2c1c954ec86f31f1a31ca85829400
Florian Lautenschlager <f.lautenschlager@gmx.net>
2021-03-22 13:41:58 +0000

    first iteration on aws. Need more storage. Approx 1.2TB for the inception preprocessing

--- initialize_aws_container.sh
+++ initialize_aws_container.sh
@@ -2,7 +2,7 @@
 
 echo "install requirements"
 pip install kaggle --upgrade
-pip install python-Levenshtein
+#pip install python-Levenshtein
 
 echo ""
 echo "input kaggle.json"
@@ -12,9 +12,9 @@ vim ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 
 echo "Download dataset"
-mkdir /data
-kaggle competitions download bms-molecular-translation -f /data/bms.zip
+#mkdir /data
+kaggle competitions download -c bms-molecular-translation -f data/bms.zip
 
-7z x /data/bms.zip -o/data/
+7z x data/bms.zip -odata/
 
 tmux new-session -s jupyter_session "jupyter lab ."
