bde6813d3394f144af6b38973b394285d016e242
Florian Lautenschlager <f.lautenschlager@gmx.net>
2021-03-22 11:43:09 +0100

    initialize-script for aws container

---
+++ initialize_aws_container.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "install requirements"
+pip install kaggle --upgrade
+pip install python-Levenshtein
+
+echo ""
+echo "input kaggle.json"
+#read KAGGLE
+#echo $KAGGLE > ~/.kaggle/kaggle.json
+vim ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+
+echo "Download dataset"
+mkdir /data
+kaggle competitions download bms-molecular-translation -f /data/bms.zip
+
+7z x /data/bms.zip -o/data/
+
